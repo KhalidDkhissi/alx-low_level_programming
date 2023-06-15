c4613ee0124d332be37f62fc6edcb763f4774302
KhalidDkhissi <khaliddkhissi@outlook.fr>
2023-06-15 05:30:13 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - entry poit
+*Return: Always 0
+*/
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,\n");
+        return 0;
+}
